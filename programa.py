95880d28aa4b0c4cc0014517f4098cf0ea3f333e
Chalarcanicolas <nicolaschalarca@colegioguanenta.edu.co>
2023-03-15 09:58:16 -0500

    Si

--- programa.py
+++ programa.py
@@ -1,25 +1,39 @@
 import random
 
-print("---------------------------------")
-print("----- LANZAMIENTO DE UN DADO ----")
-print("---------------------------------")
-
 # input
 
 # processing
-d = random(1,6)
 
-if (d==1):
-    rta =  "UNO"
-elif (d==2):
-    rta = "DOS"
-elif (d==3):
-    rta = "TRES"
-elif (d==4):
-    rta = "CUATRO"
-elif (d==5):
-    rta = "CINCO"
-elif (d==6):
-    rta = "SEIS"
+d1= random.randint(1,6)
+# d2= random.randint(1,6)
+
+d2=int(input("adivina el numero del dados"))
+
+"""if (d1==1):
+    rta= "UNO"
+elif (d1==2):
+    rta= "DOS"
+elif (d1==3):
+    rta= "TRES"
+elif (d1==4):
+    rta= "CUATRO"
+elif (d1==5):
+    rta= "CINCO"
+elif (d1==6):
+    rta= "SEIS"
 else:
-    rta = "No es la cara de un dado"
+    rta="no es la cara de un dado"""
+
+if d2>6:
+    print("USTED DIGITO UN NUMERO NO VALIDO")
+
+else:
+    if (d1==d2):
+        rta="ADIVINASTE"
+    else:
+        rta="NO ADIVINASTE"
+
+    # output
+    print(rta)
+    print("dado: " + str(d1))
+    print("USTED DIGITO: " + str(d2))
